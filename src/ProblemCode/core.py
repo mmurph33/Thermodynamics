462b2d4c773637cebb3facababdd41df2a2b165d
mmurph33 <murphbo56@gmail.com>
2023-10-28 16:52:53 -0400

    New core.py functions within PREOS class

--- src/ProblemCode/core.py
+++ src/ProblemCode/core.py
@@ -47,6 +47,37 @@ DB_PATH = "/Users/mattmurphy/Thermodynamics_Assistant/ChemE_Helper_Program/db/pr
 class PengRobinsonEOS:
     """Class containing methods for Peng-Robinson Equation of State calculations."""
     R = 8.3144621  # Universal gas constant in J/(mol·K)
+    def __init__(self, Tc, Pc, omega):
+        self.Tc = Tc
+        self.Pc = Pc
+        self.omega = omega
+
+    def kappa_pr(self):
+        """Calculate kappa based on the acentric factor."""
+        return 0.37464 + 1.54226 * self.omega - 0.26993 * self.omega**2
+
+    def alpha_pr(self, T):
+        """Calculate alpha parameter for the Peng-Robinson EOS."""
+        return (1 + self.kappa_pr() * (1 - (T / self.Tc)**0.5))**2
+
+    def apr(self, T):
+        """Calculate the attraction parameter 'a'."""
+        return self.acpr() * self.alpha_pr(T)
+
+    def bpr(self):
+        """Calculate the co-volume 'b' for the Peng-Robinson EOS."""
+        return 0.07779607 * (self.R * self.Tc / self.Pc)
+
+    def Ppr(self, T, v):
+        """Implement the Peng-Robinson equation of state."""
+        a = self.apr(T)
+        b = self.bpr()
+        return (self.R * T / v - b) - (a / (v**2 * (1 + 2 * (b / v) - ((b / v)**2))))
+
+    def acpr(self):
+        """Calculate the critical molecular attraction parameter."""
+        return 0.45723553 * (self.R * self.Tc)**2 / self.Pc
+
     @staticmethod
     def kappa(omega):
         """Calculate kappa value based on acentric factor."""
